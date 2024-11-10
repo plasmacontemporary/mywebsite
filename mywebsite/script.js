17abc9027c514ac62ba4a19fb8e3ed6aa549bdce
plasmacontemporary <ecem388m@gmail.com>
2024-11-10 22:10:13 +0000

    Add files via upload

---
+++ mywebsite/script.js
@@ -0,0 +1,4 @@
+// Script to handle button click for demonstration purposes
+document.querySelector('.contact-button').addEventListener('click', () => {
+    alert("Contact form coming soon!");
+});
